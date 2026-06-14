d1bad2fdae3d6be038ed59b5d26b21edb04e6263
dohyun <aofhoa@gmail.com>
2026-06-14 20:29:38 +0900

    chore: 백엔드 설정

--- backend/src/main/java/com/factoryflow/backend/FactoryFlowBackendApplication.java
+++ backend/src/main/java/com/factoryflow/backend/FactoryFlowBackendApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FactoryFlowBackendApplication {
 
 	public static void main(String[] args) {
+
+
+
 		SpringApplication.run(FactoryFlowBackendApplication.class, args);
 	}
 
 }
+
